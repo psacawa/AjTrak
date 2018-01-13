5729428dcd937e43dd71d351891cbf930800b5ac
psacawa <psacawa@math.toronto.edu>
2018-01-13 00:17:09 -0500

    added trainnetwork.cpp

---
+++ TrainNetwork.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <chrono>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <tiny_dnn/tiny_dnn.h>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::ofstream;
+using std::ifstream;
+using std::swap;
+
+int main () {
+	cout << "placeholder\n";
+}
